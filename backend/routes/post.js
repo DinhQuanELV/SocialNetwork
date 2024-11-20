cfa32afd2e071fdb511f6bf01ce3e27f6e55a101
Dinh Quan <quanvonguyendinh@gmail.com>
2024-11-20 21:49:40 +0700

    create my post route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -36,4 +36,15 @@ router.post('/createpost', requireLogin, (req, res) => {
     });
 });
 
+router.get('/mypost', requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user.id })
+    .populate('postedBy', '_id name')
+    .then((myPost) => {
+      res.json({ myPost });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 module.exports = router;
