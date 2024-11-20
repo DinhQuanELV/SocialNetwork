dd0a4da1c51c745579bff4298155796f754e5639
Dinh Quan <quanvonguyendinh@gmail.com>
2024-11-20 12:01:01 +0700

    config mongoose router

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const mongoose = require('mongoose');
+const port = 5000;
+
+require('./models/user');
+
+app.use(express.json());
+app.use(require('./routes/auth'));
+
+mongoose.connect(
+  'mongodb+srv://dq:6RDl85K40J979yGe@dq.ktpaz.mongodb.net/?retryWrites=true&w=majority&appName=dq',
+);
+mongoose.connection.on('connected', () => {
+  console.log('connect successfully');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('connect failure');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
