8f41b13f428f4298b36cdb4815c9e07d6320c388
Dinh Quan <quanvonguyendinh@gmail.com>
2024-12-02 10:44:56 +0700

    show latest post on top

--- backend/models/post.js
+++ backend/models/post.js
@@ -2,26 +2,31 @@ const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
 const Schema = mongoose.Schema;
 
-const Post = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  likes: [{ type: ObjectId, ref: 'User' }],
-  comments: [
-    {
-      text: String,
-      postedBy: { type: ObjectId, ref: 'User' },
+const Post = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-  ],
-  postedBy: {
-    type: ObjectId,
-    ref: 'User',
+    image: {
+      type: String,
+      required: true,
+    },
+    likes: [{ type: ObjectId, ref: 'User' }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: ObjectId, ref: 'User' },
+      },
+    ],
+    postedBy: {
+      type: ObjectId,
+      ref: 'User',
+    },
   },
-});
+  {
+    timestamps: true,
+  },
+);
 
 mongoose.model('Post', Post);
