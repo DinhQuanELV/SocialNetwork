b8c95854d253151e964f3c9836637bc3bf2782fc
Dinh Quan <quanvonguyendinh@gmail.com>
2024-12-21 20:24:10 +0700

    fix authenticate token login

--- backend/middleware/requireLogin.js
+++ backend/middleware/requireLogin.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+require('dotenv').config();
+const jwtSecretKey = process.env.JWT_SECRET_KEY;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  // authorization === Bearer wassup
   if (!authorization) {
-    res.status(401).json({ error: 'you must be login' });
+    res.status(401).json({ error: 'You must be login' });
   }
   const token = authorization.replace('Bearer ', '');
-  jwt.verify(token, 'wassup', (err, payload) => {
+  jwt.verify(token, jwtSecretKey, (err, payload) => {
     if (err || !payload) {
-      res.status(401).json({ error: 'you must be login' });
+      res.status(401).json({ error: 'You must be login' });
     }
 
     const { _id } = payload;
