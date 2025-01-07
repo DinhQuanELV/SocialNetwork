3e16072ef798cdcfe7539a3124ac63c1855f1163
Dinh Quan <quanvonguyendinh@gmail.com>
2025-01-07 12:45:02 +0700

    fix ui

--- frontend/src/components/FollowingPost/index.jsx
+++ frontend/src/components/FollowingPost/index.jsx
@@ -1,18 +1,44 @@
-import { useContext, useEffect, useState } from 'react';
+import { forwardRef, useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
-import { FaRegHeart, FaHeart, FaTrash } from 'react-icons/fa';
+import { FaRegHeart, FaHeart } from 'react-icons/fa';
+import { BsThreeDots } from 'react-icons/bs';
+import { FaRegComment } from 'react-icons/fa';
+import { Modal } from 'react-bootstrap';
+import Tippy from '@tippyjs/react/headless';
+
 import styles from './FollowingPost.module.scss';
 import { UserContext } from '~/App';
+import PostDetail from '~/components/PostDetail';
+import PostMenu from '~/components/Popovers/PostMenu';
 
 const cx = classNames.bind(styles);
 
-const FollowingPost = () => {
+const PostMenuBtn = forwardRef((props, ref) => {
+  return (
+    <button ref={ref} className={cx('menu-btn')} {...props}>
+      <BsThreeDots className={cx('menu-icon')} />
+    </button>
+  );
+});
+
+const Post = () => {
   const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+  const [visibleMenuPostId, setVisibleMenuPostId] = useState(null);
+
   const { state } = useContext(UserContext);
 
+  const handleClose = () => setShow(false);
+  const handleShow = (post) => {
+    const orderedPost = { ...post, comments: post.comments.reverse() };
+    setSelectedPost(orderedPost);
+    setShow(true);
+  };
+
   useEffect(() => {
-    fetch('/following', {
+    fetch('/post/following', {
       method: 'GET',
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('jwt'),
@@ -25,7 +51,7 @@ const FollowingPost = () => {
   }, []);
 
   const handleLikePost = (id) => {
-    fetch('/like', {
+    fetch('/post/like', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -50,7 +76,7 @@ const FollowingPost = () => {
   };
 
   const handleUnLikePost = (id) => {
-    fetch('/unlike', {
+    fetch('/post/unlike', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -74,47 +100,25 @@ const FollowingPost = () => {
       .catch((err) => console.log(err));
   };
 
-  const handleCreateComment = (text, postId) => {
-    fetch('/comment', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: 'Bearer ' + localStorage.getItem('jwt'),
-      },
-      body: JSON.stringify({
-        text,
-        postId,
-      }),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        const newData = data.map((post) => {
-          if (post._id === result._id) {
-            return result;
-          } else {
-            return post;
-          }
-        });
-        setData(newData);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleDeletePost = (postId) => {
-    fetch(`/deletepost/${postId}`, {
+  const handleRemovePost = (postId) => {
+    fetch(`/post/remove/${postId}`, {
       method: 'DELETE',
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('jwt'),
       },
     })
       .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-        const newData = data.filter((item) => {
-          return item._id !== result._id;
+      .then(() => {
+        setData((prev) => {
+          return prev.filter((p) => p._id !== postId);
         });
-        setData(newData);
       });
+  };
+
+  const handleEditPost = (updatedPost) => {
+    setData((prev) => {
+      return prev.map((p) => (p._id === updatedPost._id ? updatedPost : p));
+    });
   };
 
   return (
@@ -122,72 +126,93 @@ const FollowingPost = () => {
       {Array.isArray(data) &&
         data.map((post) => {
           return (
-            <div className={cx('post')} key={post._id}>
+            <div className={cx('post')} key={post && post._id && post._id}>
               <div className={cx('info')}>
-                <img
-                  className={cx('avatar')}
-                  src="https://lh3.googleusercontent.com/a/ACg8ocLr-WaQOOYYgxufAz56i6lS4c5fEgjmV_zPsfmVuha2wuYxZu3H=s360-c-no"
-                  alt="avatar"
-                />
                 <h4 className={cx('name')}>
                   <Link
+                    className={cx('name-link')}
                     to={
                       state && state._id && post.postedBy._id !== state._id
                         ? `/profile/${post.postedBy._id}`
                         : '/profile'
                     }
                   >
-                    {post.postedBy.name}
+                    <img
+                      className={cx('avatar')}
+                      src={post.postedBy.avatar}
+                      alt="avatar"
+                    />
+                    {post.postedBy.username}
                   </Link>
                 </h4>
-                {state && state._id && post.postedBy._id === state._id && (
-                  <span
-                    onClick={() => {
-                      handleDeletePost(post._id);
-                    }}
-                    style={{ marginLeft: 'auto' }}
-                  >
-                    <FaTrash />
-                  </span>
+                {post?.postedBy?._id === state?._id && (
+                  <div className={cx('menu')}>
+                    <Tippy
+                      visible={visibleMenuPostId === post?._id}
+                      onClickOutside={() => setVisibleMenuPostId(null)}
+                      interactive
+                      placement="bottom"
+                      render={(attrs) => (
+                        <div
+                          className={cx('avatar-option')}
+                          tabIndex="-1"
+                          {...attrs}
+                          onClick={() => setVisibleMenuPostId(null)}
+                        >
+                          <PostMenu
+                            postId={post._id}
+                            title={post.title}
+                            onDelete={() => handleRemovePost(post._id)}
+                            onEdit={handleEditPost}
+                          />
+                        </div>
+                      )}
+                    >
+                      <PostMenuBtn
+                        onClick={() => {
+                          setVisibleMenuPostId(
+                            visibleMenuPostId === post._id ? null : post._id,
+                          );
+                        }}
+                      />
+                    </Tippy>
+                  </div>
                 )}
               </div>
               <img className={cx('image')} src={post.image} alt="post" />
               <div className={cx('activity')}>
-                {state && state._id && post.likes.includes(state._id) ? (
-                  <span onClick={() => handleUnLikePost(post._id)}>
-                    <FaHeart />
-                  </span>
-                ) : (
-                  <span onClick={() => handleLikePost(post._id)}>
-                    <FaRegHeart />
-                  </span>
-                )}
-                <span>{post.likes.length} likes</span>
-                <span>{post.title}</span>
-                <span>
-                  {post.comments.map((comment) => {
-                    return (
-                      <h6 key={comment._id}>
-                        <span>{comment.postedBy.name}</span>
-                        {comment.text}
-                      </h6>
-                    );
-                  })}
-                </span>
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    handleCreateComment(e.target[0].value, post._id);
-                  }}
-                >
-                  <input type="text" placeholder="Add a comment..." />
-                </form>
+                <div>
+                  {state && state._id && post.likes.includes(state._id) ? (
+                    <span onClick={() => handleUnLikePost(post._id)}>
+                      <FaHeart className={cx('like-icon-active')} />
+                    </span>
+                  ) : (
+                    <span onClick={() => handleLikePost(post._id)}>
+                      <FaRegHeart className={cx('like-icon')} />
+                    </span>
+                  )}
+                  <span className={cx('likes')}>{post.likes.length}</span>
+                  <FaRegComment
+                    className={cx('comment-icon')}
+                    onClick={() => handleShow(post)}
+                  />
+                  <span className={cx('comments')}>{post.comments.length}</span>
+                </div>
+                <span className={cx('caption')}>{post.title}</span>
               </div>
             </div>
           );
         })}
+      <Modal show={show} onHide={handleClose} centered data-bs-theme="dark">
+        <Modal.Header closeButton>
+          <Modal.Title>Comments</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {selectedPost && <PostDetail post={selectedPost} setData={setData} />}
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
 
-export default FollowingPost;
+export default Post;
--- frontend/src/components/Pages/FollowUsersToSee/index.jsx
+++ frontend/src/components/Pages/FollowUsersToSee/index.jsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind';
 import styles from './FollowUsersToSee.module.scss';
+import FollowUserToSee from '~/assets/images/FollowUserToSee.png';
 
 const cx = classNames.bind(styles);
 
@@ -8,6 +9,7 @@ const FollowUsersToSee = () => {
     <div className={cx('wrapper')}>
       <h2 className={cx()}>You are not following anyone </h2>
       <h2 className={cx()}>Follow more users to see their photos</h2>
+      <img src={FollowUserToSee} alt="Follow User To See" />
     </div>
   );
 };
